fe479934d571ab08c39a079d20a9b7216de890a6
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2022-03-28 10:37:46 +0530

    added urls for the blog

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { application } = require("express");
+const _ = require("lodash");
 
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
@@ -53,7 +53,17 @@ app.post("/compose", function (req,res) {
 })
 
 app.get('/posts/:postName', function(req, res)  {  // makes dynamic website urls using express
-console.log(req.params.postName );
+let requestedTitle = _.lowerCase(req.params.postName); 
+posts.forEach(function (post) {
+  let storedTitle = _.lowerCase(post.title);
+  if (storedTitle === requestedTitle) {
+    res.render("post" , {
+      title: post.title ,
+      content: post.content
+  
+    });
+  }
+})
 });
  
 
